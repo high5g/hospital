5ffcbd7e3ab43d36d6db71e4db5bf3ee7aa604eb
seifeddine_dridi <seifeddine.dridi89@gmail.com>
2015-01-24 13:58:46 +0100

    Added database SQL script

---
+++ SQL/high5_hospital.sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 24, 2015 at 01:57 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `high5_hospital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chambre`
+--
+
+CREATE TABLE IF NOT EXISTS `chambre` (
+  `id` int(11) NOT NULL,
+  `frais_chambre` int(11) NOT NULL,
+  `etat` enum('0','1') NOT NULL,
+  `fk_id_hopital` int(11) NOT NULL,
+  KEY `fk_id_hopital` (`fk_id_hopital`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facture`
+--
+
+CREATE TABLE IF NOT EXISTS `facture` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fk_id_patient` int(11) NOT NULL,
+  `fk_id_medecin` int(11) NOT NULL,
+  `prix` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_id_patient` (`fk_id_patient`),
+  KEY `fk_id_medecin` (`fk_id_medecin`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hopital`
+--
+
+CREATE TABLE IF NOT EXISTS `hopital` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) NOT NULL,
+  `adresse` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medecin`
+--
+
+CREATE TABLE IF NOT EXISTS `medecin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `specialite` varchar(100) NOT NULL,
+  `fk_id_personne` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `fk_id_personne` (`fk_id_personne`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicament`
+--
+
+CREATE TABLE IF NOT EXISTS `medicament` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(30) NOT NULL,
+  `categorie` varchar(30) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE IF NOT EXISTS `patient` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_couverture_sociale` int(11) NOT NULL,
+  `date_entree` date NOT NULL,
+  `date_sortie` date NOT NULL,
+  `fk_id_personne` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `fk_id_personne` (`fk_id_personne`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personne`
+--
+
+CREATE TABLE IF NOT EXISTS `personne` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) NOT NULL,
+  `prenom` varchar(30) NOT NULL,
+  `cin` int(11) NOT NULL,
+  `adresse` varchar(250) NOT NULL,
+  `telephone` int(11) NOT NULL,
+  `classe` enum('0','1','2') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `chambre`
+--
+ALTER TABLE `chambre`
+  ADD CONSTRAINT `chambre_ibfk_1` FOREIGN KEY (`fk_id_hopital`) REFERENCES `hopital` (`id`);
+
+--
+-- Constraints for table `facture`
+--
+ALTER TABLE `facture`
+  ADD CONSTRAINT `facture_ibfk_3` FOREIGN KEY (`fk_id_medecin`) REFERENCES `medecin` (`id`),
+  ADD CONSTRAINT `facture_ibfk_1` FOREIGN KEY (`fk_id_patient`) REFERENCES `patient` (`id`),
+  ADD CONSTRAINT `facture_ibfk_2` FOREIGN KEY (`fk_id_patient`) REFERENCES `patient` (`id`);
+
+--
+-- Constraints for table `medecin`
+--
+ALTER TABLE `medecin`
+  ADD CONSTRAINT `medecin_ibfk_1` FOREIGN KEY (`fk_id_personne`) REFERENCES `personne` (`id`);
+
+--
+-- Constraints for table `patient`
+--
+ALTER TABLE `patient`
+  ADD CONSTRAINT `patient_ibfk_1` FOREIGN KEY (`fk_id_personne`) REFERENCES `personne` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
